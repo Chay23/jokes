447d310e8e89039495386d487264b27145b09d66
Nazar Shcherbii <nazar.shcherbii@greenice.net>
2025-06-25 17:19:38 +0300

    Update global modal component

--- src/components/modals/Modal.tsx
+++ src/components/modals/Modal.tsx
@@ -1,4 +1,4 @@
-import type { SxProps } from '@mui/material';
+import type { ModalProps, SxProps } from '@mui/material';
 import type { Theme } from '@mui/system';
 
 import { useAppSelector } from '../../hooks/redux/hooks';
@@ -6,23 +6,14 @@ import { useAppSelector } from '../../hooks/redux/hooks';
 import ModalWrapper from '@mui/material/Modal';
 import Box from '@mui/material/Box';
 
-const style: SxProps<Theme> = {
-	position: 'absolute',
-	top: '50%',
-	left: '50%',
-	transform: 'translate(-50%, -50%)',
-	minWidth: 400,
-	bgcolor: 'background.paper',
-	p: 4,
-};
-
 type Props = {
 	id: string;
 	children: React.ReactNode;
 	onClose: () => void;
-};
+	sx?: SxProps<Theme>;
+} & Omit<ModalProps, 'children' | 'open' | 'onClose' | 'sx'>;
 
-export default function Modal({ id, children, onClose }: Props) {
+export default function Modal({ id, children, sx, onClose, ...props }: Props) {
 	const modal = useAppSelector((state) => state.modals.modals[id]);
 
 	if (!modal) {
@@ -30,8 +21,22 @@ export default function Modal({ id, children, onClose }: Props) {
 	}
 
 	return (
-		<ModalWrapper open={modal.isOpen} onClose={onClose}>
-			<Box sx={style}>{children}</Box>
+		<ModalWrapper open={modal.isOpen} onClose={onClose} {...props}>
+			<Box
+				sx={{
+					position: 'absolute',
+					top: '50%',
+					left: '50%',
+					transform: 'translate(-50%, -50%)',
+					minWidth: 250,
+					bgcolor: 'background.paper',
+					p: 4,
+					borderRadius: 3,
+					...sx,
+				}}
+			>
+				{children}
+			</Box>
 		</ModalWrapper>
 	);
 }
